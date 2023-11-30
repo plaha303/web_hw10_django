a772d81bed530fd592944facf19f7ecefa570065
plaha303 <111688671+plaha303@users.noreply.github.com>
2023-11-30 21:23:05 +0200

    added email password reset tool

--- hw10_project/users/urls.py
+++ hw10_project/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView
 from django.urls import path
 from . import views
 
@@ -6,5 +7,20 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.signupuser, name='signup'),
     path('login/', views.loginuser, name='login'),
-    path('logout/', views.logoutuser, name='logout')
+    path('logout/', views.logoutuser, name='logout'),
+    path('reset-password/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('reset-password/done/',
+         PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>',
+         PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html',
+             success_url='/users/reset-password/complete',
+         ),
+         name='password_reset_confirm'),
+    path('reset-password/complete/',
+         PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
